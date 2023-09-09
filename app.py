8dfe6ff2921951296624112eee7dc1969832465e
Лена Проворова <prlprovorova@yandex.ru>
2023-09-09 16:38:27 +0700

    Вывод страницы первой лабораторной

--- app.py
+++ app.py
@@ -3,4 +3,22 @@ app = Flask(__name__)
 
 @app.route("/")
 def start():
-    return "web-сервер на flask"
+    return """
+<!doctype html>
+<html>
+    <head>
+        <title>Крамар Д.Н., Проворова Е.А., Лабораторная 1</title>
+    </head>
+    <body>
+        <header>
+            НГТУ, ФБ, Лабораторная работа 1
+        </header>
+        
+        <h1>web-сервер на flask</h1>
+
+        <footer>
+            &copy;Крамар Д.Н., Проворова Е.А., ФБИ-11, 3 курс, 2023   
+        </footer>
+    </body>
+</html>
+"""
