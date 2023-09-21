2b78c45b5e8a5d9739218ec19cd3078ea93eb2ac
Лена Проворова <prlprovorova@yandex.ru>
2023-09-21 21:11:46 +0700

    Статические файлы

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect
+from flask import Flask, redirect, url_for
 app = Flask(__name__)
 
 @app.route("/")
@@ -54,3 +54,20 @@ def lab1():
     </body>
 </html>
 """
+
+@app.route('/lab1/oak')
+def oak():
+    return '''
+<!doctype html>
+<html style="background-color:rgb(210, 228, 248)">
+<link rel="stylesheet" href="''' + url_for('static', filename = 'lab1.css') + '''">
+    <head>
+        <title>Крамар Д.Н., Проворова Е.А., Лабораторная 1</title>
+    </head>
+    <body>
+        <h1 style="text-align: center; font-style:italic">Дуб</h1>
+        <img src="''' + url_for('static', filename = 'oak.jpg') + '''">
+    </body>
+</html>
+'''
+
