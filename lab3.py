404945ade917ac39d74f90c2e633ccce6148cd49
Лена Проворова <prlprovorova@yandex.ru>
2023-10-19 15:21:38 +0700

    Заказ напитка

--- lab3.py
+++ lab3.py
@@ -1,9 +1,11 @@
-from flask import Blueprint, url_for, render_template, request
+from flask import Blueprint, url_for, render_template, request, redirect
 lab3 = Blueprint('lab3', __name__)
+
 
 @lab3.route("/lab3/")
 def lab():
     return render_template ('lab3.html')
+
 
 @lab3.route("/lab3/form1")
 def form1():
@@ -18,3 +20,33 @@ def form1():
 
     sex = request.args.get('sex')
     return render_template ('form1.html', user=user, age=age, sex=sex, errors=errors)
+
+
+@lab3.route("/lab3/order")
+def order():
+    return render_template ('order.html')
+
+
+@lab3.route("/lab3/pay")
+def pay():
+    price = 0
+    drink = request.args.get('drink')
+    if drink == 'cofee':
+        price = 120
+    elif drink == 'black-tea':
+        price = 80
+    elif drink == 'green-tea':
+        price = 70
+
+    if request.args.get('milk') == 'on':
+        price += 30
+    if request.args.get('sugar') == 'on':
+        price += 10
+
+    return render_template ('pay.html', price=price)
+
+
+@lab3.route("/lab3/success")
+def success():
+    return render_template ('success.html')
+    
