44c9a8e065e9a657af8c42e5743e1684d537f3b4
Лена Проворова <prlprovorova@yandex.ru>
2023-11-15 17:24:46 +0700

    Холодильник

--- lab4.py
+++ lab4.py
@@ -25,3 +25,33 @@ def login():
     error = 'Неверные логин и/или пароль'
     return render_template ('login.html', username = username, 
                             password = password, errors = errors, error = error)
+
+
+@lab4.route("/lab4/fridge", methods = ['GET', 'POST'])
+def fridge():
+    if request.method == 'GET':
+        return render_template ('fridge.html')
+    error = {}
+    result = {}
+    result2 = {}
+    temp = request.form.get('temp')
+    if temp == '' :
+        error = 'Ошибка: не задана температура'
+        result = ''
+        result2 = ''
+    elif int(temp) < -12 :
+        result = 'Не удалось установить температуру - слишком низкое значение'
+        result2 = ''
+    elif int(temp) > -1 :
+        result = 'Не удалось установить температуру - слишком высокое значение'
+        result2 = ''
+    elif -12 <= int(temp) <= -9:
+        result = f'Установлена температура: {temp} °C'
+        result2 = '***'
+    elif -8 <= int(temp) <= -5:
+        result = f'Установлена температура: {temp} °C'
+        result2 = '**'
+    else:
+        result = f'Установлена температура: {temp} °C'
+        result2 = '*'
+    return render_template ('fridge.html', temp = temp, error=error, result=result, result2=result2)
