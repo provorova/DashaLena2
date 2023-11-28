f32278195900d5171f07268d839414af8040c2d1
Лена Проворова <prlprovorova@yandex.ru>
2023-11-28 22:42:37 +0700

    Вывод пользователей

--- lab5.py
+++ lab5.py
@@ -1,34 +1,51 @@
 from flask import Blueprint, render_template, request
 import psycopg2
+
 lab5 = Blueprint('lab5', __name__)
 
-
-@lab5.route("/lab5/")
-def main():
-    #параметры для подключения к БД
-    conn = psycopg2.connect (
+def dbConnect():
+    conn = psycopg2.connect(
         host = "127.0.0.1",
         database = "knowledge_base",
         user = "provorova_knowledge_base",
-        password = "123"
-    )
-    # получаем курсор. С помощью него мы можем выполнять SQL-запросы
+        password = "123")
+
+    return conn;
+
+def dbClose(cursor, connection):
+    # закрываем курсор и соединение
+    # порядок важен!
+    cursor.close()
+    connection.close()
+
+@lab5.route("/lab5/")
+def main():
+    conn = dbConnect()
     cur = conn.cursor()
 
-    # получаем курсор. спомощью него мы можем выполнять SQL-запросы
     cur.execute("SELECT * FROM users;")
 
-    # fetchal - получить все строки, которые получились в результате
-    # выполение SQL-запроса в execute 
-    # созраняем эти строки в переменную result
     result = cur.fetchall()
-
-    # закрываем соединение с БД
-    cur.close()
-    conn.close()
 
     print(result)
 
+    dbClose(cur, conn)      
+
     return "go to console"
 
-   
+
+@lab5.route("/lab5/users")
+def users():
+    conn = dbConnect()
+    cur = conn.cursor()
+
+    cur.execute("SELECT * FROM users;")
+
+    result = cur.fetchall()
+
+    names = [row[1] for row in result]
+
+    dbClose(cur, conn)
+
+    return render_template('users.html', names = names)
+
