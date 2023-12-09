546bcc177307019f6ed95d53679c53f0b392a5cf
Лена Проворова <prlprovorova@yandex.ru>
2023-12-09 20:26:40 +0700

    Заготовка к 7-ой лабораторной

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from lab3 import lab3
 from lab4 import lab4
 from lab5 import lab5
 from lab6 import lab6
+from lab7 import lab7
 from Db import db
 from Db.models import users
 from flask_login import LoginManager
@@ -39,3 +40,4 @@ app.register_blueprint(lab3)
 app.register_blueprint(lab4)
 app.register_blueprint(lab5)
 app.register_blueprint(lab6)
+app.register_blueprint(lab7)
