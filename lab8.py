c1ef0d8f2c9f1a85cb96f7b86c8d51b3292283bd
Лена Проворова <prlprovorova@yandex.ru>
2023-12-15 23:30:39 +0700

    Удаление курса

--- lab8.py
+++ lab8.py
@@ -6,6 +6,7 @@ lab8 = Blueprint('lab8', __name__)
 def main():
     return render_template('lab8/index.html')
 
+
 courses = [
     {"name": "c++", "videos": 3, "price": 3000},
     {"name": "basic", "videos": 30, "price": 0},
@@ -13,16 +14,17 @@ courses = [
 ]
 
 
-@lab8.route('/lab8/api/courses', methods=['GET'])
+@lab8.route('/lab8/api/courses/', methods=['GET'])
 def get_courses():
     return courses
+
 
 @lab8.route('/lab8/api/courses/<int:course_num>', methods=['GET'])
 def get_course(course_num):
     if course_num >= 0 and course_num < (len(courses)):
         return courses[course_num]
     else:
-        abort (404)
+        abort(404)
 
 
 @lab8.route('/lab8/api/courses/<int:course_num>', methods=['DELETE'])
@@ -31,7 +33,7 @@ def del_course(course_num):
         del courses[course_num]
         return '', 204
     else:
-        abort (404)
+        abort(404)
 
 
 @lab8.route('/lab8/api/courses/<int:course_num>', methods=['PUT'])
@@ -41,11 +43,12 @@ def put_course(course_num):
         courses[course_num] = course
         return courses[course_num]
     else:
-        abort (404)
+        abort(404)
 
 
-@lab8.route('/lab8/api/courses', methods=['POST'])
-def add_courses():
+@lab8.route('/lab8/api/courses/', methods=['POST'])
+def add_course():
     course = request.get_json()
     courses.append(course)
-    return {"num": len(course)-1}
+    return {"num":len(courses)-1}
+    
---
+++ static/lab8/main.js
@@ -0,0 +1,52 @@
+function fillCourseList() {
+    fetch('/lab8/api/courses/')
+    .then(function(data) {
+        return data.json();
+    })
+    .then(function(courses) {
+        let tbody = document.getElementById('course-list');
+        tbody.innerHTML = '';
+        for(let i = 0; i<courses.length; i++) {
+            tr = document.createElement('tr');
+
+            let tdName = document.createElement('td');
+            tdName.innerText = courses[i].name;
+            let tdVideos = document.createElement('td');
+            tdVideos.innerText = courses[i].videos;
+            let tdPrice = document.createElement('td');
+            tdPrice.innerText = courses[i].price || 'Бесплатно!';
+
+            let editButton = document.createElement('button');
+            editButton.innerText = 'Редактировать';
+            editButton.onclick = function() {
+                editCourse(i, courses[i]);
+            }
+            let delButton = document.createElement('button');
+            delButton.innerText = 'Удалить';
+            delButton.onclick = function() {
+                deleteCourse(i);
+            };
+            let tdActions = document.createElement('td');
+            tdActions.append(editButton);
+            tdActions.append(delButton);
+  
+
+            tr.append(tdName);
+            tr.append(tdVideos);
+            tr.append(tdPrice);
+            tr.append(tdActions);
+
+            tbody.append(tr);
+        }
+    })
+}
+
+function deleteCourse(num) {
+    if(! confirm('Вы точно хотите удалить курс?'))
+        return;
+    
+    fetch(`/lab8/api/courses/${num}`, {method: 'DELETE'})
+    .then(function () {
+        fillCourseList();
+    })
+}
