f96b91feb688ee94b1527d47f65c58740ae98d00
Лена Проворова <prlprovorova@yandex.ru>
2023-12-16 00:38:44 +0700

    Добавление курса

--- static/lab8/main.js
+++ static/lab8/main.js
@@ -30,7 +30,6 @@ function fillCourseList() {
             tdActions.append(editButton);
             tdActions.append(delButton);
   
-
             tr.append(tdName);
             tr.append(tdVideos);
             tr.append(tdPrice);
@@ -49,4 +48,44 @@ function deleteCourse(num) {
     .then(function () {
         fillCourseList();
     })
+}
+
+function showModal() {
+    document.querySelector('div.modal').style.display = 'block';
+}
+
+function hideModal() {
+    document.querySelector('div.modal').style.display = 'none';
+}
+
+function cancel() {
+    hideModal();
+}
+
+function addCourse() {
+    document.getElementById('num').value = '';
+    document.getElementById('name').value = '';
+    document.getElementById('videos').value = '';
+    document.getElementById('price').value = '';
+    showModal();
+}
+
+function sendCourse() {
+    const course = {
+        name: document.getElementById('name').value,
+        videos: document.getElementById('videos').value,
+        price: document.getElementById('price').value
+    }
+
+    const url = `/lab8/api/courses/`;
+    const method = 'POST';
+    fetch(url, {
+        method: method,
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(course)
+    })
+    .then(function() {
+        fillCourseList();
+        hideModal();
+    });
 }
