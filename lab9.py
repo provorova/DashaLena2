2c037eb2dbc832dc8751b56aba15748952c0fd3b
Лена Проворова <prlprovorova@yandex.ru>
2023-12-25 20:59:21 +0700

    Обработчик ошибки 404

--- lab9.py
+++ lab9.py
@@ -5,3 +5,8 @@ lab9 = Blueprint('lab9', __name__)
 @lab9.route('/lab9/')
 def main():
     return render_template('lab9/index.html')
+
+
+@lab9.app_errorhandler(404)
+def not_found(e):
+    return render_template('lab9/error.html'), 404
